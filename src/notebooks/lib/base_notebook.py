136ba950ba5cc933d57631b5abbb1da424ee8ff4
rcbravo-dev <81670967+rcbravo-dev@users.noreply.github.com>
2024-01-24 17:40:18 +0700

    adjusting gitignore

--- src/notebooks/lib/base_notebook.py
+++
@@ -1,8 +0,0 @@
-from lib.utils import setup_logging
-logging = setup_logging(path = 'configs/logging_config.yaml')
-LOG = logging.getLogger('notebook')
-LOG.debug('notebook log.')
-
-
-def foo(x):
-    return x + 1
