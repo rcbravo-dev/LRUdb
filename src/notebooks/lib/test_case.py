cd4caae64d4ee40331733e1508fa39028a668dc5
rcbravo-dev <81670967+rcbravo-dev@users.noreply.github.com>
2024-02-03 09:28:39 +0700

    added test case to gen random read/writes

---
+++ src/notebooks/lib/test_case.py
@@ -0,0 +1,110 @@
+"""
+Animation of Elastic collisions with Gravity
+
+author: Jake Vanderplas
+email: vanderplas@astro.washington.edu
+website: http://jakevdp.github.com
+license: BSD
+Please feel free to use and modify this, but keep the above information. Thanks!
+
+http://jakevdp.github.io/blog/2012/08/18/matplotlib-animation-tutorial/
+"""
+
+import numpy as np
+from scipy.spatial.distance import pdist, squareform
+
+
+class ParticleBox:
+    """Orbits class
+    
+    init_state is an [N x 4] array, where N is the number of particles:
+       [[x1, y1, vx1, vy1],
+        [x2, y2, vx2, vy2],
+        ...               ]
+
+    bounds is the size of the box: [xmin, xmax, ymin, ymax]
+    """
+    def __init__(self,
+                 init_state = [[1, 0, 0, -1],
+                               [-0.5, 0.5, 0.5, 0.5],
+                               [-0.5, -0.5, -0.5, 0.5]],
+                 bounds = [-2, 2, -2, 2],
+                 size = 0.04,
+                 M = 0.05,
+                 G = 9.8):
+        self.init_state = np.asarray(init_state, dtype=float)
+        self.M = M * np.ones(self.init_state.shape[0])
+        self.size = size
+        self.state = self.init_state.copy()
+        self.time_elapsed = 0
+        self.bounds = bounds
+        self.G = G
+
+    def step(self, dt):
+        """step once by dt seconds"""
+        self.time_elapsed += dt
+        
+        # update positions
+        self.state[:, :2] += dt * self.state[:, 2:]
+
+        # find pairs of particles undergoing a collision
+        D = squareform(pdist(self.state[:, :2]))
+        ind1, ind2 = np.where(D < 2 * self.size)
+        unique = (ind1 < ind2)
+        ind1 = ind1[unique]
+        ind2 = ind2[unique]
+
+        # update velocities of colliding pairs
+        for i1, i2 in zip(ind1, ind2):
+            # mass
+            m1 = self.M[i1]
+            m2 = self.M[i2]
+
+            # location vector
+            r1 = self.state[i1, :2]
+            r2 = self.state[i2, :2]
+
+            # velocity vector
+            v1 = self.state[i1, 2:]
+            v2 = self.state[i2, 2:]
+
+            # relative location & velocity vectors
+            r_rel = r1 - r2
+            v_rel = v1 - v2
+
+            # momentum vector of the center of mass
+            v_cm = (m1 * v1 + m2 * v2) / (m1 + m2)
+
+            # collisions of spheres reflect v_rel over r_rel
+            rr_rel = np.dot(r_rel, r_rel)
+            vr_rel = np.dot(v_rel, r_rel)
+            v_rel = 2 * r_rel * vr_rel / rr_rel - v_rel
+
+            # assign new velocities
+            self.state[i1, 2:] = v_cm + v_rel * m2 / (m1 + m2)
+            self.state[i2, 2:] = v_cm - v_rel * m1 / (m1 + m2) 
+
+        # check for crossing boundary
+        crossed_x1 = (self.state[:, 0] < self.bounds[0] + self.size)
+        crossed_x2 = (self.state[:, 0] > self.bounds[1] - self.size)
+        crossed_y1 = (self.state[:, 1] < self.bounds[2] + self.size)
+        crossed_y2 = (self.state[:, 1] > self.bounds[3] - self.size)
+
+        self.state[crossed_x1, 0] = self.bounds[0] + self.size
+        self.state[crossed_x2, 0] = self.bounds[1] - self.size
+
+        self.state[crossed_y1, 1] = self.bounds[2] + self.size
+        self.state[crossed_y2, 1] = self.bounds[3] - self.size
+
+        self.state[crossed_x1 | crossed_x2, 2] *= -1
+        self.state[crossed_y1 | crossed_y2, 3] *= -1
+
+        # add gravity
+        self.state[:, 3] -= self.M * self.G * dt
+
+        # RANDOM Read/Write - rcbravo
+        self.reads = [x for x in ind1]
+        self.writes = {}
+        for i in ind2:
+            self.writes[i] = self.state[i, :2]
+
